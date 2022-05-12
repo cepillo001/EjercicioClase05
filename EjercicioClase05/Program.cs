33bb3652235ce5fb2dd96c3447fb36f512d124bb
Jose Luis <Jose Luis@PCFX8350>
2022-05-12 16:20:33 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioClase05/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Ejercicio 1 - Clase 05 */
+
+string respuesta = "S";
+string nombre;
+
+while (respuesta.ToUpper() == "S")
+{
+    Console.Clear();
+    Console.Write("Ingrese su Nombre: ");
+    nombre = Console.ReadLine();
+
+    Console.WriteLine($"¡Hola {nombre}!");
+
+    Console.Write("Desea continuar? (S/N): ");
+    respuesta = Console.ReadLine();
+}
+
+if (respuesta.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente");
+}
+else
+{
+    Console.WriteLine("Opcion no Valida");
+}
